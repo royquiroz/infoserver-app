6ecd2c0b56260565ba67e0ec1e0622472dc2f050
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-09-14 16:39:19 -0500

    Se termina con la parte de discos

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -13,6 +13,8 @@ export class InfoComponent implements OnInit {
   /**Informacion por notaria */
   infoId: any;
   dataId: boolean = false;
+  dateNow: any = new Date();
+  discLength: any
 
   ngOnInit() {
     this.searchData();
@@ -40,7 +42,7 @@ export class InfoComponent implements OnInit {
     this._serversService.dataId(id).subscribe(
       (res: any) => {
         this.infoId = res.data;
-        console.log(this.infoId);
+        this.discLength = this.infoId.info.length
       },
       err => {
         console.log(err);
@@ -50,7 +52,6 @@ export class InfoComponent implements OnInit {
 
   selectNotaria(id) {
     if (id === "0" || id === "") {
-      console.log("Seleccionaste la opcion todos");
       this.dataId = false;
     } else {
       this.dataId = true;
@@ -61,4 +62,128 @@ export class InfoComponent implements OnInit {
   isVisible() {
     this.visible = !this.visible;
   }
+
+
+  /*Funciones para establecer colores a los campos de discos*/
+
+  porcentYellow(path: string, porcentaje: any, avail: any) {
+
+    if(path === '/' || path === '/mnt/datos/share'){
+      porcentaje = porcentaje.split("%")[0];
+      porcentaje = parseInt(porcentaje);
+      if (porcentaje >= 50 && porcentaje <= 79) {
+        return true;
+      }
+      return
+    }
+    avail = avail.replace('G', '')
+    avail = parseInt(avail)
+    //console.log(path, avail)
+    if (avail > 100 && avail <= 200) {
+      return true;
+    }
+    return false
+  }
+
+  porcentRed(path: string, porcentaje: any, avail: any) {
+
+    if(path === '/' || path === '/mnt/datos/share'){
+      porcentaje = porcentaje.split("%")[0];
+      porcentaje = parseInt(porcentaje);
+      if (porcentaje >= 80 && porcentaje <= 100) {
+        return true;
+      }
+      return
+    }
+    avail = avail.replace('G', '')
+    avail = parseInt(avail)
+    //console.log(path, avail)
+    if (avail <= 100) {
+      return true;
+    }
+    return false;
+
+  }
+
+  values(path: string, porcentaje: any, avail: any){
+    if(path === '/' || path === '/mnt/datos/share'){
+      return porcentaje
+    }
+    return avail
+  }
+  /* ********************************************************************* */
+
+  /*availYellow(path: any, avail: any) {
+    if(path != '/' || path != '/mnt/datos/share'){
+      return
+    }
+    avail = avail.replace('G', '')
+    avail = parseInt(avail)
+    console.log(avail)
+    if (avail > 100 && avail <= 200) {
+      return true;
+    }
+    return false;
+  }*/
+
+  /*availRed(path: any, avail: any) {
+    if(path != '/' || path != '/mnt/datos/share'){
+      return
+    }
+    avail = avail.replace('G', '')
+    avail = parseInt(avail)
+    console.log(avail)
+    if (avail <= 100) {
+      return true;
+    }
+    return false;
+  }*/
+
+  /*Funcion para separar el nombre de el fylesystem */
+
+  cutName(path: any){
+    path = path.split('/')
+    let lengthPath = path.length
+    path = path[lengthPath-1]
+    if(path === '') {
+      return 'raiz'
+    }
+    return path
+  }
+
+
+
+
+  /*
+  oneDayBefore(date: any) {
+    if (date === "" || date === null || date === undefined) {
+      return;
+    }
+    date = new Date(date);
+
+    if (date < this.dateNow) {
+      return true;
+    }
+    return false;
+  }
+
+  maintenance(date: any){
+    if (date === "" || date === null || date === undefined) {
+      return;
+    }
+    date = new Date(date);
+
+    if(date.getMonth() < this.dateNow.getMonth() && date.getFullYear() <= this.dateNow.getFullYear()){
+      return true
+    }
+    return false
+  }
+
+  hours(date: any){
+    if (date === "" || date === null || date === undefined) {
+      return;
+    }
+    console.log(date.getHours())
+
+  }*/
 }
